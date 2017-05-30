c5a64dead89e678d7c932113a1aa12cd5bc3ca30
parkerziegler <parkerellisziegler@gmail.com>
2017-05-29 20:12:34 -0700

    Adding Khula font from Google Fonts API, setting up flexbox.

--- src/components/maps/MapDescription.js
+++ src/components/maps/MapDescription.js
@@ -12,7 +12,7 @@ class MapDescription extends React.Component {
 
 			return (
 				<div className='map-description-container'>
-					<div className='header'>Shootings Per Million By State</div>
+					<div className='inset-header'>Shootings Per Million By State</div>
 					<div className='text'>Between January 1, 2015 and July 9, 2016, 1,715people were killed by police in the United States. That amounts to roughly 3.1 people per day. New Mexico had the highest normalized rate of police involved shootings in this time period, with 17.26 for every million residents. Hover over a state to obtain its rate.
 					</div>
 				</div>
