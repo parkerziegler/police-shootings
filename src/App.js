e0ce8a36ce77b54be9472e961b672a97df8c2a08
parkerziegler <parkerellisziegler@gmail.com>
2017-08-24 20:02:08 -0700

    Moving API calls to saga invoked in App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import Header from './components/Header';
 import Map from './components/maps/Map';
 import MapDescription from './components/maps/MapDescription';
+import * as actions from './actions/actions';
 
 class App extends Component {
+
+  componentWillMount() {
+    this.props.dispatch(actions.callAPI());
+  }
+
   render() {
 
     return (
@@ -12,14 +19,15 @@ class App extends Component {
         <Header />
 				<div className='percapita'>
 	        <Map mapType='choropleth' />
-					<MapDescription />
+					<MapDescription mapType='choropleth' insetHeader='Shootings per Million' />
 				</div>
         <div className='raw'>
           <Map mapType='proportional' />
+          <MapDescription mapType='choropleth' insetHeader='Total Shootings'/>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
