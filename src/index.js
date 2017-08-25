cadda40b8e9779b90b4661638b74e4c9a6db365e
parkerziegler <parkerellisziegler@gmail.com>
2017-08-24 20:02:22 -0700

    Adding redux-saga middleware to reducer.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { rootReducer } from './reducers/index';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const reducers = combineReducers({ rootReducer });
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
