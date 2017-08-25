f82e2b7a0be7106230db1b05e0ec0dbc7b806dae
parkerziegler <parkerellisziegler@gmail.com>
2017-08-24 20:03:55 -0700

    Action type for calling api.

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,4 +1,5 @@
 // Action types for our reducer
+export const CALL_API = 'CALL_API';
 export const SEND_API_DATA_TO_REDUCER = 'SEND_API_DATA_TO_REDUCER';
 export const SEND_SHOOTINGS_DATA_TO_REDUCER = 'SEND_SHOOTINGS_DATA_TO_REDUCER';
 export const GET_HOVERED_STATE_DATA = 'GET_HOVERED_STATE_DATA';
