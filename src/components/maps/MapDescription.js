1d768608d7e57e07fd0ed96ed9661e7014a93df8
parkerziegler <parkerellisziegler@gmail.com>
2017-08-24 20:03:37 -0700

    Moving calculations to separate methods.

--- src/components/maps/MapDescription.js
+++ src/components/maps/MapDescription.js
@@ -13,22 +13,103 @@ const FirstChild = (props) => {
 
 class MapDescription extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.getStatsForChoropleth = this.getStatsForChoropleth.bind(this);
+		this.getStatsForProportional = this.getStatsForProportional.bind(this);
+	}
+
+	getStatsForChoropleth() {
+
+		// first calculate how many shootings occurred per day on average
+		let totalCount = Number(this.props.maps.shootingsData.length).toLocaleString();
+		
+		// use moment to get the difference between start and end date
+		let numDays = moment("2016-12-31").diff(moment("2015-01-01"), "days");
+
+		// get the average
+		let numPerDay = Number(this.props.maps.shootingsData.length / numDays).toFixed(1);
+
+		// also calculate the highest state
+		let highestState = this.props.maps.data.objects ?
+		_.orderBy(this.props.maps.data.objects.states.geometries, ['properties.shootingsPerCapita'], ['desc']) : null;
+
+		// get the name of the state with the highest rate
+		let highestStateName = highestState ? highestState[0].properties.stateName : 'Loading...';
+
+		// get its rate as well
+		let highestRate = highestState ? Number(highestState[0].properties.shootingsPerCapita * 1000000).toFixed(1) : 'loading...'
+
+		// finally, get the state that the user is hovering
+		let activeState = this.props.maps.activeState ? this.props.maps.activeState : null;
+
+		// put everything in an object to return
+		let choroplethStats = {
+			totalCount,
+			numPerDay,
+			highestStateName,
+			highestRate,
+			activeState
+		};
+
+		return choroplethStats;
+
+	}
+
+	getStatsForProportional() {
+
+		// calculate the highest state
+		let highestState = this.props.maps.data.objects ?
+		_.orderBy(this.props.maps.data.objects.states.geometries, ['properties.numberShootings'], ['desc']) : null;
+
+		let highestCount = highestState ? highestState[0].properties.numberShootings : 'loading...';
+
+		let secondHighest = highestState ? highestState[1].properties.stateName : null;
+
+		let thirdHighest = highestState ? highestState[2].properties.stateName : null;
+
+		let lowestState = highestState ? highestState[-1].properties.stateName : null;
+
+		let secondLowest = highestState ? highestState[-2].properties.stateName : null;
+
+		return {
+			highestState,
+			highestCount,
+			secondHighest,
+			thirdHighest,
+			lowestState,
+			secondLowest
+		};
+
+	}
+
+	getJSXForChoropleth() {
+
+		let stats = this.getStatsForChoropleth();
+
+		return (
+			<div className='text'>
+				Between January 1, 2015 and December 31, 2016, <b>{stats.totalCount}</b> people were killed by police in the United States. That amounts to roughly <b>{stats.numPerDay}</b> people per day. <br /><br /><b>{stats.highestStateName}</b> had the highest rate of police involved shootings in this time period, with <b>{stats.highestRate}</b> for every million residents.<br/> <br />Hover over a state to obtain its rate.
+			</div>
+		);
+
+
+	}
+
+	getJSXForProportional() {
+
+		let stats = this.getStatsForProportional();
+
+		return (
+			<div className='text'>
+				Of all 50 states, <b>{stats.highestState}</b> had the greatest number of shootings in this time period at <b>{stats.highestCount}</b>. <b>{stats.secondHighest}</b> and <b>{stats.thirdHighest}</b>
+			</div>
+		)
+	}
+
 	render() {
 
-			let totalCount = Number(this.props.maps.shootingsData.length).toLocaleString();
-
-			let numDays = moment("2016-12-31").diff(moment("2015-01-01"), "days");
-
-			let numPerDay = Number(this.props.maps.shootingsData.length / numDays).toFixed(1);
-
-			let highestState = this.props.maps.data.objects ?
-					_.orderBy(this.props.maps.data.objects.states.geometries, ['properties.shootingsPerCapita'], ['desc']) : null;
-
-			let highestStateName = highestState ? highestState[0].properties.stateName : 'Loading...';
-
-			let highestRate = highestState ? Number(highestState[0].properties.shootingsPerCapita * 1000000).toFixed(1) : 'loading...'
-
-			let activeState = this.props.maps.activeState ? this.props.maps.activeState : null;
+		let insetJSX = this.props.mapType === 'choropleth' ? this.getJSXForChoropleth() : this.getJSXForProportional();
 
 			return (
 				<CSSTransitionGroup
@@ -39,12 +120,11 @@ class MapDescription extends React.Component {
 				transitionLeave={false}
 				component={FirstChild}>
 					<div className='map-description-container'>
-						<div className='inset-header'>Shootings Per Million</div>
+						<div className='inset-header'>{this.props.insetHeader}</div>
 						<div className='inset-subheader'>By State</div>
-						<div className='text'>Between January 1, 2015 and December 31, 2016, <b>{totalCount}</b> people were killed by police in the United States. That amounts to roughly <b>{numPerDay}</b> people per day. <br /><br /><b>{highestStateName}</b> had the highest rate of police involved shootings in this time period, with <b>{highestRate}</b> for every million residents.<br/> <br />Hover over a state to obtain its rate.
-						</div>
-						<div className='state-name'>{activeState.stateName ? activeState.stateName : 'New Mexico'}</div>
-						<div className='inset-subheader'>{activeState.shootingsPerMillion ? _.round(activeState.shootingsPerMillion, 2) : 20.66} shootings per million</div>
+						{insetJSX}
+						{/*<div className='state-name'>{stats.activeState.stateName ? stats.activeState.stateName : 'New Mexico'}</div>
+						<div className='inset-subheader'>{stats.activeState.shootingsPerMillion ? _.round(stats.activeState.shootingsPerMillion, 2) : 20.66} shootings per million</div>*/}
 						<DataTable />
 					</div>
 				</CSSTransitionGroup>
