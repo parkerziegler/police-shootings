27a3610e1e27382111ef62e71ff3deda5e23bd4a
parkerziegler <parkerellisziegler@gmail.com>
2017-08-27 13:43:21 -0700

    Adding a fetching flag to Redux, updating it via sagas.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import * as actionTypes from '../constants/action-types';
 
 const initialState = {
+	fetchingData: true,
   data: [{}],
 	shootingsData: [{}],
 	activeState: {}
@@ -8,9 +9,16 @@ const initialState = {
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+		case actionTypes.CALL_API:
+			return {...state,
+				fetchingData: true
+			};
+		case actionTypes.REUQEST_TOPOJSON_DATA:
+			return state;
     case actionTypes.SEND_API_DATA_TO_REDUCER:
       return Object.assign({}, state, {
-        data: action.data.us
+				data: action.data,
+				fetchingData: false
       });
 		case actionTypes.SEND_SHOOTINGS_DATA_TO_REDUCER:
 			return Object.assign({}, state, {
