ac6121f556d98b9feed8bac09c1f0aa431c75a69
parkerziegler <parkerellisziegler@gmail.com>
2017-09-09 17:33:33 -0700

    Simplifying our root index file.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
+// import necessary modules
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { rootReducer } from './reducers/index';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/rootSaga';
 
+// create our saga middleware - we'll use this handle our side effects
+// seamlessly
 const sagaMiddleware = createSagaMiddleware();
 
-const reducers = combineReducers({ rootReducer });
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+// create our redux store, applying the saga middleware
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+// be sure to run the saga middleware
 sagaMiddleware.run(rootSaga);
 
+// render our App
 ReactDOM.render(
   <Provider store={store}>
     <App />
