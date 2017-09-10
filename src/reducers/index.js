b834030dc9b980ebf809cf289ffd3382eb31e108
parkerziegler <parkerellisziegler@gmail.com>
2017-09-09 17:34:08 -0700

    Breaking out reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,38 +1,4 @@
-import * as actionTypes from '../constants/action-types';
+import { combineReducers } from 'redux';
+import { mapReducer } from './mapReducer';
 
-const initialState = {
-	fetchingData: true,
-  data: [{}],
-	shootingsData: [{}],
-	activeState: {
-		stateName: 'New Mexico',
-		shootings: 43,
-		shootingsPerMillion: 20.66
-	}
-};
-
-export const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-		case actionTypes.CALL_API:
-			return {...state,
-				fetchingData: true
-			};
-		case actionTypes.REUQEST_TOPOJSON_DATA:
-			return state;
-    case actionTypes.SEND_API_DATA_TO_REDUCER:
-      return Object.assign({}, state, {
-				data: action.data,
-				fetchingData: false
-      });
-		case actionTypes.SEND_SHOOTINGS_DATA_TO_REDUCER:
-			return Object.assign({}, state, {
-				shootingsData: action.data
-			});
-		case actionTypes.GET_HOVERED_STATE_DATA:
-			return Object.assign({}, state, {
-				activeState: action.state
-			});
-    default:
-      return state;
-  }
-}
+export default combineReducers({ mapReducer });
---
+++ src/reducers/mapReducer.js
@@ -0,0 +1,40 @@
+// import necessary modules
+import * as actionTypes from '../constants/action-types';
+
+// define our initial reducer state
+const initialState = {
+	fetchingData: true,
+    geoData: [],
+	shootingsData: [],
+	activeState: {
+		stateName: 'New Mexico',
+		shootings: 43,
+		shootingsPerMillion: 20.66
+	}
+};
+
+export const mapReducer = (state = initialState, action) => {
+  switch (action.type) {
+		case actionTypes.CALL_API:
+			return {...state,
+				fetchingData: true
+			};
+		case actionTypes.REQUEST_TOPOJSON_DATA:
+			return state;
+        case actionTypes.SEND_API_DATA_TO_REDUCER:
+            return {...state,
+                geoData: action.data,
+                fetchingData: false
+            };
+		case actionTypes.SEND_SHOOTINGS_DATA_TO_REDUCER:
+			return { ...state,
+				shootingsData: action.data
+			};
+		case actionTypes.GET_HOVERED_STATE_DATA:
+			return { ...state,
+				activeState: action.state
+			};
+        default:
+            return state;
+  }
+};
