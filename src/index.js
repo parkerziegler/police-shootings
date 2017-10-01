01fb07b0603ed15f58de619c81052d8ef83aad8c
parkerziegler <parkerellisziegler@gmail.com>
2017-10-01 13:29:05 -0700

    Updates to route objects.

--- src/index.js
+++ src/index.js
@@ -13,19 +13,43 @@ import rootSaga from './sagas/rootSaga';
 const routes = {
   '/': {
     title: 'Intro',
-    index: 0
+    index: 0,
+    navClass: 'main'
   },
   '/total-shootings': {
     title: 'Total Shootings',
-    index: 1
+    index: 1,
+    navClass: 'main',
+    hasChildren: true,
+    '/black': {
+      title: 'Total Shootings By Race - African American',
+      navClass: 'child',
+      childIndex: 0,
+      hasNextSibling: true
+    },
+    '/latino': {
+      title: 'Total Shootings By Race - Latino',
+      navClass: 'child',
+      childIndex: 1,
+      hasNextSibling: true
+    },
+    '/white': {
+      title: 'Total Shootings By Race - White',
+      navClass: 'child',
+      childIndex: 2,
+      hasNextSibling: false
+    }
   },
   '/percapita': {
     title: 'Shootings Per Million By State',
-    index: 2
+    index: 2,
+    navClass: 'main'
   },
   '/shootingsbydate': {
     title: 'Shootings By Date',
-    index: 3
+    index: 3,
+    navClass: 'main',
+    isLastRoute: true
   }
 };
 
@@ -35,7 +59,7 @@ const {
   enhancer
 } = routerForBrowser({
   routes,
-  basename: '/d3-react-map/'
+  basename: '/d3-react-map'
 });
 
 // create our saga middleware - we'll use this handle our side effects
@@ -51,13 +75,11 @@ const store = createStore(
 // be sure to run the saga middleware
 sagaMiddleware.run(rootSaga);
 
+// get the initial location of the router
 const initialLocation = store.getState().router;
 if (initialLocation) {
-  console.log(initialLocation);
   store.dispatch(initializeCurrentLocation(initialLocation));
 }
-
-console.log(process.env);
 
 // render our App
 ReactDOM.render(
