c8b090496e721d1b44f9cf11160005fcacced4cd
parkerziegler <parkerellisziegler@gmail.com>
2017-10-08 20:15:34 -0700

    Creating titles for user navigation on Chevron element.

--- src/components/navigation/Chevron/Chevron.jsx
+++ src/components/navigation/Chevron/Chevron.jsx
@@ -5,13 +5,18 @@ import './Chevron.css';
 
 const Chevron = (props) => {
 
+    let style = props.nextSlide ? {flexDirection: 'column'} : null;
+
     return (
         <Link href={props.href} className={props.className}>
-            <div className='chevron'>
+            <div className='chevron' style={style}>
+                {props.nextSlide && props.visible ?
+                <div className='slide-title'>{props.nextSlide}</div> : null}
                 {props.visible ?
-                <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                <svg width="40" height="40" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                     <path fill="#6C7680" d={props.path}/>
                 </svg> : null}
+                
             </div>
         </Link>
     );
@@ -21,7 +26,8 @@ Chevron.propTypes = {
     href: PropTypes.string.isRequired,
     path: PropTypes.string.isRequired,
     className: PropTypes.string.isRequired,
-    visible: PropTypes.bool.isRequired
+    visible: PropTypes.bool.isRequired,
+    nextSlide: PropTypes.string
 };
 
 export default Chevron;
