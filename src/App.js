d130c010c864175928aed7f243822c1816ba64fd
parkerziegler <parkerellisziegler@gmail.com>
2017-10-08 20:14:03 -0700

    Sass changes on App.

--- src/App.js
+++ src/App.js
@@ -196,7 +196,7 @@ class App extends Component {
             </Fragment>
             <Chevron className='chevron-link horizontal' path="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" href={nextMainRoute} visible={nextMainRoute !== '/'} />
           </div>
-          <Chevron className='chevron-link vertical' path="M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z" href={nextChildRoute} visible={nextChildRoute !== '/'} />
+          <Chevron className='chevron-link vertical' path="M1683 808l-742 741q-19 19-45 19t-45-19l-742-741q-19-19-19-45.5t19-45.5l166-165q19-19 45-19t45 19l531 531 531-531q19-19 45-19t45 19l166 165q19 19 19 45.5t-19 45.5z" href={nextChildRoute} visible={nextChildRoute !== '/'} nextSlide={router.routes[nextChildRoute].title}/>
         </div>
       </Fragment>;
 
