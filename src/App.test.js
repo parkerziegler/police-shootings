8e39723d094c93e1415438249781b68f8bb017ae
parkerziegler <parkerellisziegler@gmail.com>
2017-11-11 09:16:35 -0800

    Reconfiguring tests with setupTests, React 16, Enzyme 3.

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Provider } from 'redux';
 import { store } from './index';
 import Enzyme, { shallow } from 'enzyme';
 import App from './App';
-import Adapter from 'enzyme-adapter-react-15';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   shallow(
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <App store={store} />
   );
 });
