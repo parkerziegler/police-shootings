f1f8ad9446e264ebe4525f416daf35272adef1cc
parkerziegler <parkerellisziegler@gmail.com>
2017-11-16 21:06:25 -0800

    Adding more tests for Home component.

--- src/components/home/Home.test.js
+++ src/components/home/Home.test.js
@@ -8,3 +8,15 @@ it('renders without crashing', () => {
       <Home store={store} />
   );
 });
+
+it('renders all expected child divs', () => {
+
+  const wrapper = shallow(<Home store={store} />);
+
+  // example of how to debug output
+  console.log(wrapper.debug());
+  console.log(wrapper.dive().debug());
+  console.log(wrapper.dive().find('.page-content').children().debug());
+  
+  expect(wrapper.dive().find('.page-content').children()).toHaveLength(6);
+});
