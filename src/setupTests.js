c8f375869d1ecfcc5c08bab9fbf4c3e0aeb22811
parkerziegler <parkerellisziegler@gmail.com>
2017-11-22 16:20:58 -0800

    Integrating Jest snapshot testing.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {createSerializer} from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
 
 global.shallow = shallow;
 global.React = React;
+
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
