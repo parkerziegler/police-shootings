8175e7dbb7e243cda19d975107ef91bad7840c2f
parkerziegler <parkerellisziegler@gmail.com>
2018-01-02 14:05:47 -0800

    Python for generating json of The Counted data.

--- python/csv-to-json.py
+++ python/csv-to-json.py
@@ -1,18 +1,27 @@
+# import csv
+# import json
+
+# csv2015 = open('the-counted-2015.csv', 'r')
+# csv2016 = open('the-counted-2016.csv', 'r')
+# jsonfile = open('shootings-data.json', 'w')
+
+# fieldnames = ("uid", "name", "age", "gender", "raceethnicity", "month", "day", "year", "streetaddress", "city", "state", "classification", "lawenforcementagency", "armed")
+
+# reader2015 = csv.DictReader(csv2015, fieldnames)
+# next(reader2015, None)
+# out2015 = json.dumps([row for row in reader2015])
+
+# reader2016 = csv.DictReader(csv2016, fieldnames)
+# next(reader2015, None)
+# out2016 = json.dumps([row for row in reader2016])
+# out = out2015 + out2016
+# jsonfile.write(out)
+
 import csv
 import json
+import pandas as pd
 
-csv2015 = open('the-counted-2015.csv', 'r')
-csv2016 = open('the-counted-2016.csv', 'r')
-jsonfile = open('shootings-data.json', 'w')
-
-fieldnames = ("uid", "name", "age", "gender", "raceethnicity", "month", "day", "year", "streetaddress", "city", "state", "classification", "lawenforcementagency", "armed")
-
-reader2015 = csv.DictReader(csv2015, fieldnames)
-next(reader2015, None)
-for row in reader2015:
-    json.dump(row, jsonfile)
-
-reader2016 = csv.DictReader(csv2016, fieldnames)
-next(reader2015, None)
-for row in reader2016:
-    json.dump(row, jsonfile)
+data2015 = pd.read_csv('the-counted-2015.csv', sep=",")
+data2016 = pd.read_csv('the-counted-2016.csv', sep=",")
+data = data2015.append(data2016)
+data.to_json('../public/shootings-data.json', orient="records")
