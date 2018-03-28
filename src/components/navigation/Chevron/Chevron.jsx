98d9bde86be29712fd9d481323e3811eb9a2aab4
parkerziegler <parker.ziegler@formidable.com>
2018-03-27 18:28:59 -0700

    Rewrite Chevron component to be simpler.

--- src/components/navigation/Chevron/Chevron.jsx
+++ src/components/navigation/Chevron/Chevron.jsx
@@ -1,33 +1,22 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'redux-little-router';
-import './Chevron.css';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { go } from "redux-little-router";
+import "./Chevron.css";
 
-const Chevron = (props) => {
-
-    let style = props.nextSlide ? {flexDirection: 'column'} : null;
-
-    return (
-        <Link href={props.href} className={props.className}>
-            <div className='chevron' style={style}>
-                {props.nextSlide && props.visible ?
-                <div className='slide-title'>{props.nextSlide}</div> : null}
-                {props.visible ?
-                <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                    <path fill="#6C7680" d={props.path}/>
-                </svg> : null}
-                
-            </div>
-        </Link>
-    );
+const Chevron = ({ className, onClick, path }) => {
+  return (
+    <div className="chevron" onClick={onClick}>
+      <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#6C7680" d={path} />
+      </svg>
+    </div>
+  );
 };
 
 Chevron.propTypes = {
-    href: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired,
-    className: PropTypes.string.isRequired,
-    visible: PropTypes.bool.isRequired,
-    nextSlide: PropTypes.string
+  path: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
 export default Chevron;
