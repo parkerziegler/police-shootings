c13e137b488320398c63e347a28fe4fe565c424b
parkerziegler <parker.ziegler@formidable.com>
2018-04-04 09:34:46 -0700

    Update tests.

--- __tests__/Home.spec.js
+++ __tests__/Home.spec.js
@@ -6,16 +6,6 @@ describe('<Home />', () => {
     shallow(<Home store={store} />);
   });
 
-  it('renders all expected child divs', () => {
-    const wrapper = shallow(<Home store={store} />);
-    expect(
-      wrapper
-        .dive()
-        .find('.page-content')
-        .children()
-    ).toHaveLength(6);
-  });
-
   it('matches the Jest snapshot', () => {
     const wrapper = mount(<Home store={store} />);
     expect(wrapper.html()).toMatchSnapshot();
--- __tests__/Map.spec.js
+++ __tests__/Map.spec.js
@@ -1,7 +1,8 @@
 import { store } from '../src/index';
 import Map from '../src/components/maps/Map/Map';
 
-it('renders without crashing', () => {
-
+describe('<Map />', () => {
+  it('renders without crashing', () => {
     shallow(<Map mapType="choropleth" store={store} />);
+  });
 });
