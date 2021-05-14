7cd1bde1f7c5807021cd16c82424de8d4cb5fe05
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-14 18:33:50 +0000

    Deploy parkerziegler/police-shootings to github.com/parkerziegler/police-shootings.git:gh-pages

---
+++ precache-manifest.72bb3655344f94ee45d2f1998c532743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395624bf663585f60e57aca67202dfae",
+    "url": "/police-shootings/index.html"
+  },
+  {
+    "revision": "74a9e582bfbe5492ebfd",
+    "url": "/police-shootings/static/css/main.400771cf.chunk.css"
+  },
+  {
+    "revision": "20b14477d229104720c2",
+    "url": "/police-shootings/static/js/2.15618868.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/police-shootings/static/js/2.15618868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a9e582bfbe5492ebfd",
+    "url": "/police-shootings/static/js/main.d0cf301c.chunk.js"
+  },
+  {
+    "revision": "e532395550467ec87238",
+    "url": "/police-shootings/static/js/runtime-main.fbe87c9c.js"
+  }
+]);
